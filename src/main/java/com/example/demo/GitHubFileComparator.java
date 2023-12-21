f158ec74e0c948917244879697d4402b2e2e26f2
Rafee Shaik <rafee.shaik@oportun.com>
2023-12-21 21:51:17 +0530

    test 1

--- src/main/java/com/example/demo/GitHubFileComparator.java
+++ src/main/java/com/example/demo/GitHubFileComparator.java
@@ -12,7 +12,7 @@ public class GitHubFileComparator {
         String owner = "reachrafee";
         String repo = "demo";
         String filePath = "src/main/resources/features.json";
-        String token = "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"; // Replace with your GitHub token
+        String token = "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"; // Replace with your GitHub token
 
         try {
             String masterBranchContent = getFileContent(owner, repo, filePath, "main", token);
